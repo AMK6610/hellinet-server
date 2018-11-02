aecf530d28fe54ff6731fefadc0af8e69106db3b
Ali <a.hoseinmardy@gmail.com>
2018-11-02 20:44:11 +0330

    Add results to excel file

--- Server/runCode.py
+++ Server/runCode.py
@@ -1,4 +1,6 @@
 import os
+import openpyxl
+from openpyxl.styles import Font
 from subprocess import Popen , PIPE
 
 def runCodesTogether(file,otherfile,fileAddress,otherfileAddress):
@@ -8,67 +10,147 @@ def runCodesTogether(file,otherfile,fileAddress,otherfileAddress):
                     stdout=PIPE, stdin=PIPE)
     out, err = fight.communicate()
     fight.wait()
+    return out
     # print(out)
 
 os.chdir("..")
 directory = os.getcwd()
+excelDir = directory
 
-for file in os.listdir(directory + "\Uploads\python"):
-    os.chdir(directory + "\Uploads\python")
+wb = openpyxl.load_workbook('OutputExcel.xlsx')
+sheet = wb['Sheet1']
+sheet.row_dimensions[1].height = 20
+sheet.row_dimensions[1].width = 500
+
+sheet['A1'] = 'Team 1'
+sheet['B1'] = 'Team 2'
+fontObj1 = Font(name='Times New Roman', bold=True)
+sheet['A1'].font = fontObj1
+sheet['B1'].font = fontObj1
+
+columncounter = 2;
+
+wb.save('OutputExcel.xlsx')
+
+for file in os.listdir(directory + "\\Uploads\python"):
+    os.chdir(directory + "\\Uploads\python")
     fileAddress = os.getcwd();
     if file.endswith(".py"):
-        for otherfile in os.listdir(directory + "\Uploads\python"):
-            os.chdir(directory + "\Uploads\python")
+        for otherfile in os.listdir(directory + "\\Uploads\python"):
+            os.chdir(directory + "\\Uploads\python")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".py") and otherfile != file:
-                runCodesTogether(file,otherfile,fileAddress,otherfileAddress)
-        for otherfile in os.listdir(directory + "\Uploads\exe"):
-            os.chdir(directory + "\Uploads\exe")
+                result = runCodesTogether(file,otherfile,fileAddress,otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
+        for otherfile in os.listdir(directory + "\\Uploads\exe"):
+            os.chdir(directory + "\\Uploads\exe")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".exe"):
-                runCodesTogether(file, otherfile,fileAddress,otherfileAddress)
-        for otherfile in os.listdir(directory + "\Uploads\pascal"):
-            os.chdir(directory + "\Uploads\pascal")
+                result = runCodesTogether(file, otherfile,fileAddress,otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
+        for otherfile in os.listdir(directory + "\\Uploads\pascal"):
+            os.chdir(directory + "\\Uploads\pascal")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".pas"):
-                runCodesTogether(file, otherfile, fileAddress, otherfileAddress)
+                result = runCodesTogether(file, otherfile, fileAddress, otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
 
-for file in os.listdir(directory + "\Uploads\exe"):
-    os.chdir(directory + "\Uploads\exe")
+for file in os.listdir(directory + "\\Uploads\exe"):
+    os.chdir(directory + "\\Uploads\exe")
     fileAddress = os.getcwd();
     if file.endswith(".exe"):
-        for otherfile in os.listdir(directory + "\Uploads\python"):
-            os.chdir(directory + "\Uploads\python")
+        for otherfile in os.listdir(directory + "\\Uploads\python"):
+            os.chdir(directory + "\\Uploads\python")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".py"):
-                runCodesTogether(file,otherfile,fileAddress,otherfileAddress)
-        for otherfile in os.listdir(directory + "\Uploads\exe"):
-            os.chdir(directory + "\Uploads\exe")
+                result = runCodesTogether(file,otherfile,fileAddress,otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
+        for otherfile in os.listdir(directory + "\\Uploads\exe"):
+            os.chdir(directory + "\\Uploads\exe")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".exe") and otherfile != file:
-                runCodesTogether(file, otherfile,fileAddress,otherfileAddress)
-        for otherfile in os.listdir(directory + "\Uploads\pascal"):
-            os.chdir(directory + "\Uploads\pascal")
+                result = runCodesTogether(file, otherfile,fileAddress,otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
+        for otherfile in os.listdir(directory + "\\Uploads\pascal"):
+            os.chdir(directory + "\\Uploads\pascal")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".pas"):
-                runCodesTogether(file, otherfile, fileAddress, otherfileAddress)
+                result = runCodesTogether(file, otherfile, fileAddress, otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
 
-for file in os.listdir(directory + "\Uploads\pascal"):
-    os.chdir(directory + "\Uploads\pascal")
+for file in os.listdir(directory + "\\Uploads\pascal"):
+    os.chdir(directory + "\\Uploads\pascal")
     fileAddress = os.getcwd();
     if file.endswith(".pas"):
-        for otherfile in os.listdir(directory + "\Uploads\python"):
-            os.chdir(directory + "\Uploads\python")
+        for otherfile in os.listdir(directory + "\\Uploads\python"):
+            os.chdir(directory + "\\Uploads\python")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".py"):
-                runCodesTogether(file,otherfile,fileAddress,otherfileAddress)
-        for otherfile in os.listdir(directory + "\Uploads\exe"):
-            os.chdir(directory + "\Uploads\exe")
+                result = runCodesTogether(file,otherfile,fileAddress,otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
+        for otherfile in os.listdir(directory + "\\Uploads\exe"):
+            os.chdir(directory + "\\Uploads\exe")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".exe"):
-                runCodesTogether(file, otherfile,fileAddress,otherfileAddress)
-        for otherfile in os.listdir(directory + "\Uploads\pascal"):
-            os.chdir(directory + "\Uploads\pascal")
+                result = runCodesTogether(file, otherfile,fileAddress,otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
+        for otherfile in os.listdir(directory + "\\Uploads\pascal"):
+            os.chdir(directory + "\\Uploads\pascal")
             otherfileAddress = os.getcwd()
             if otherfile.endswith(".pas") and otherfile != file:
-                runCodesTogether(file, otherfile, fileAddress, otherfileAddress)
+                result = runCodesTogether(file, otherfile, fileAddress, otherfileAddress)
+                if result == 1:
+                    sheet.cell(row=columncounter, column=1).value = "Winner"
+                    sheet.cell(row=columncounter, column=2).value = "Loser"
+                else:
+                    sheet.cell(row=columncounter, column=1).value = "Loser"
+                    sheet.cell(row=columncounter, column=2).value = "Winner"
+                columncounter += 1
