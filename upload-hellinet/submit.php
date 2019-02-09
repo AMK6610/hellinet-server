ee111deaa0bd5790daaecd37006c79284f8de72b
AMK6610 <amir.karimi6610@gmail.com>
2019-02-09 18:11:08 +0330

    minor fix

--- upload-hellinet/submit.php
+++ upload-hellinet/submit.php
@@ -48,21 +48,23 @@
                 if($flag == $cnt && $flag != 0) {
                     echo "<div class='col align-self-center alert alert-success' role='alert' style='height: 100px; text-align: center; margin-top: 300px; padding-top: 40px;'>
                     Your file(s) are successfully uploaded!<br>
-                    Running ...
+                    Redirecting ...
                     </div>";
 //                    $command = escapeshellcmd('python ../Server/runCode.py');
-                    if ($cnt > 0) {
-                        for ($i = 0; $i < count($_FILES['upload']['name']); $i++) {
-                            $output = shell_exec("python ../Server/runCode.py" . $_FILES['upload']['name'][$i] . "2>&1");
-                            //                    echo $output;
-                        }
-                        header("Location: leaderboard.php");
-                    }
+//                    if ($cnt > 0) {
+//                        for ($i = 0; $i < count($_FILES['upload']['name']); $i++) {
+//                            $output = shell_exec("python ../Server/runCode.py" . $_FILES['upload']['name'][$i] . "2>&1");
+//                            //                    echo $output;
+//                        }
+//                        header("Location: leaderboard.php");
+//                    }
                 }
                 else
                     echo "<div class='col align-self-center alert alert-danger' role='alert' style='height: 100px; text-align: center; margin-top: 300px; padding-top: 40px;'>
                     Could not upload your file! A problem detected
+                    Redirecting ...
                     </div>";
+                header("refresh:5;url=index.php");
             ?>
         </div>
     </body>
