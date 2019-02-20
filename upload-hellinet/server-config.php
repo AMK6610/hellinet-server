2c7fdc65bcc705421f43ea06550d49da394db911
AMK6610 <amir.karimi6610@gmail.com>
2019-02-20 13:46:45 +0330

    add server config

---
+++ upload-hellinet/server-config.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_SERVER', 'http://gamestep.firststep.ir');
+define('DB_USERNAME', 'c83user');
+define('DB_PASSWORD', 'gamestephellinet1397');
+define('DB_DATABASE', 'c83db');
+date_default_timezone_set("Asia/Tehran");
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+/**
+ * Created by PhpStorm.
+ * User: Shinigami
+ * Date: 1/31/2019
+ * Time: 6:12 PM
+ */
